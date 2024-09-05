6aba22ee2271a3fe0a38d0a0d0e75637aae9b9e3
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-09-04 22:59:03 -0400

    plugin's basic structure

--- src/QuestPlugin.hs
+++ src/QuestPlugin.hs
@@ -2,11 +2,30 @@
 --
 module QuestPlugin (plugin) where
 
-import GHC.Plugins
-import MonadicBang.Internal
+import GHC.Plugins as GHC
+import GHC.Hs
+import GHC
+
+import qualified Data.Generics as SYB
 
 plugin :: Plugin
 plugin = defaultPlugin
   { parsedResultAction = rewriteQuest
   , pluginRecompile = purePlugin
   }
+
+rewriteQuest ::[CommandLineOption] -> ModSummary -> ParsedResult -> Hsc ParsedResult
+rewriteQuest _ _ orig@(ParsedResult m _)
+  = do
+    dflags <- GHC.getDynFlags
+    hpm_module' <- transform dflags (GHC.hpm_module m)
+    pure $ orig { GHC.parsedResultModule = m { GHC.hpm_module = hpm_module' } }
+
+transform
+    :: GHC.DynFlags
+    -> GHC.Located (HsModule GhcPs)
+    -> GHC.Hsc (GHC.Located (HsModule GhcPs))
+transform _dflags = SYB.everywhereM (SYB.mkM transform')
+  where
+    transform' :: LHsExpr GhcPs -> GHC.Hsc (LHsExpr GhcPs)
+    transform' = pure
